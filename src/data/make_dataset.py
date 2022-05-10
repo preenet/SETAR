d2f08bd9010b6632974c153bfed6c12f9a07f78e
preenet <preenet@gmail.com>
2022-05-10 11:08:43 +0700

    added make thaitale datatset.

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,48 +1,76 @@
 
 """
-Loading and making raw data 
+Loading original and making raw data 
 @Authors: pree.t@cmu.ac.th
-Todo:   
+Todo:
 * add google drive or one drive 
 """
-import os
+import sys
 import pandas as pd
 import src.utilities as utils
 
 # get config file
 config = utils.read_config()
-data_path_ws_ori = os.path.join(config['data']['raw_ws_ori'])
-data_path_ws = os.path.join(config['data']['raw_ws'])
+config['data']['raw_ws_ori']
+config['data']['raw_ws']
 
-print('making khon-thai corpus...')
+def make_kt():
+    print('Making khon-thai corpus...')
+    return
 
-print('making wisesight corpus...')
-# we use the original wisesight corpus and reconstruct a new dataframe
-texts = []
-targets = []
+def make_ws():
+    print('Making wisesight corpus...')
+    texts = []
+    targets = []
+    with open(config['data']['raw_ws_ori'] + '/' + 'neg.txt', encoding='utf-8') as f:
+        for line in f:
+            texts.append(line.strip())
+            targets.append('neg')
 
-with open(str(data_path_ws_ori) + '/' + 'neg.txt', encoding='utf-8') as f:
-    for line in f:
-        texts.append(line.strip())
-        targets.append('neg')
+    with open(config['data']['raw_ws_ori'] + '/' + 'neu.txt', encoding='utf-8') as f:
+        for line in f:
+            texts.append(line.strip())
+            targets.append('neu')
 
-with open(str(data_path_ws_ori) + '/' + 'neu.txt', encoding='utf-8') as f:
-    for line in f:
-        texts.append(line.strip())
-        targets.append('neu')
+    with open(config['data']['raw_ws_ori'] + '/' + 'pos.txt', encoding='utf-8') as f:
+        for line in f:
+            texts.append(line.strip())
+            targets.append('pos')
 
-with open(str(data_path_ws_ori) + '/' + 'pos.txt', encoding='utf-8') as f:
-    for line in f:
-        texts.append(line.strip())
-        targets.append('pos')
+    with open(config['data']['raw_ws_ori'] + '/' + 'q.txt', encoding='utf-8') as f:
+        for line in f:
+            texts.append(line.strip())
+            targets.append('q')
+            
+    df_ws = pd.DataFrame({'texts': texts, 'targets': targets})
+    print(df_ws.shape)
+    df_ws.to_csv(config['data']['raw_ws'])
+   
+    return
 
-with open(str(data_path_ws_ori) + '/' + 'q.txt', encoding='utf-8') as f:
-    for line in f:
-        texts.append(line.strip())
-        targets.append('q')
-        
-df_ws = pd.DataFrame({'texts': texts, 'targets': targets})
-print(df_ws.shape)
-print("writing to disk..")
-df_ws.to_csv(data_path_ws)
-print('make dataset finished!')
+def make_tt():
+    print('Making thaitale corpus...')
+    df_tt = pd.read_csv(config['data']['raw_tt_ori'] + 'tale_data.csv')
+    print(df_tt.shape)
+    df_tt.to_csv(config['data']['raw_tt'] + 'thaitale.csv')
+    return
+
+if __name__ == "__main__":
+
+    if (len(sys.argv) != 2):
+        print("need argument")
+        sys.exit(1)
+
+    if sys.argv[1] in config['data']['name']: 
+        data_name = sys.argv[1]
+
+        if data_name == 'kt':
+            make_kt()
+        elif data_name == 'ws':
+            make_ws()
+        elif data_name == 'tt':
+            make_tt()
+    else:
+        print("no such data name.")
+        sys.exit(1)
+print('Program terminate sucessfully!')
