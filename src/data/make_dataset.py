08fb6d26599fd11205faab2f685e568d80650e0c
preenet <preenet@gmail.com>
2022-05-10 11:38:37 +0700

    updated make datasets

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,6 +1,6 @@
 
 """
-Loading original and making raw data 
+Loading original corupus and making a standard experiment raw dataset.
 @Authors: pree.t@cmu.ac.th
 Todo:
 * add google drive or one drive 
@@ -11,6 +11,10 @@ import src.utilities as utils
 
 def make_kt():
     print('Making khon-thai corpus...')
+    df_kt = pd.read_csv(config['data']['raw_kt_ori'])
+    df_kt.rename(columns={'vote': 'target'}, inplace=True)
+    print(df_kt.info)
+    df_kt.to_csv(config['data']['raw_kt'])
     return
 
 def make_ws():
@@ -21,33 +25,39 @@ def make_ws():
         for line in f:
             texts.append(line.strip())
             targets.append('neg')
+    f.close()
 
     with open(config['data']['raw_ws_ori'] + '/' + 'neu.txt', encoding='utf-8') as f:
         for line in f:
             texts.append(line.strip())
             targets.append('neu')
+    f.close()
 
     with open(config['data']['raw_ws_ori'] + '/' + 'pos.txt', encoding='utf-8') as f:
         for line in f:
             texts.append(line.strip())
             targets.append('pos')
+    f.close()
 
     with open(config['data']['raw_ws_ori'] + '/' + 'q.txt', encoding='utf-8') as f:
         for line in f:
             texts.append(line.strip())
             targets.append('q')
+    f.close()
             
     df_ws = pd.DataFrame({'texts': texts, 'targets': targets})
-    print(df_ws.shape)
+    df_ws.rename(columns={'texts': 'text', 'targets': 'target'}, inplace=True)
+    print(df_ws.info)
     df_ws.to_csv(config['data']['raw_ws'])
    
     return
 
 def make_tt():
     print('Making thaitale corpus...')
-    df_tt = pd.read_csv(config['data']['raw_tt_ori'] + 'tale_data.csv')
-    print(df_tt.shape)
-    df_tt.to_csv(config['data']['raw_tt'] + 'thaitale.csv')
+    df_tt = pd.read_csv(config['data']['raw_tt_ori'])
+    df_tt.rename(columns={'consensus': 'target'}, inplace=True)
+    print(df_tt.info)
+    df_tt.to_csv(config['data']['raw_tt'])
     return
 
 if __name__ == "__main__":
@@ -68,6 +78,6 @@ if __name__ == "__main__":
         elif data_name == 'tt':
             make_tt()
     else:
-        print("no such data name.")
+        print("Error: no such data name.")
         sys.exit(1)
 print('Program terminate sucessfully!')
