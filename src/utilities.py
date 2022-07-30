a5a8bd245fd058555324bc200b9f99b9899f2665
preenet <preenet@gmail.com>
2022-07-30 10:33:06 +0700

    added W2V results kt.

--- src/utilities.py
+++ src/utilities.py
@@ -76,6 +76,6 @@ def generate_report_10repeated_deepbaseline(data_name, file_name):
 
 if __name__ == "__main__":
     config = read_config()
-    generate_report_10repeated_baseline('W2V_WS', '12classifier_W2VTFIDF_(1, 1)_ws.csv')
+    generate_report_10repeated_baseline('POS_TAG_KT', '12classifier_POSBOWCONCAT_(1, 1)_kt.csv')
     #generate_report_10repeated_deepbaseline('BLSTM_WS', 'blstm_10repeated_ws.csv')
     
